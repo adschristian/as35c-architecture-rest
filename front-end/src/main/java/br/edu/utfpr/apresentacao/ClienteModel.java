0b59a1ddf6777e2e4cd26e7391fb599800e6eecb
Christian Alves <christian.alvesdasilva@gmail.com>
2019-06-16 18:12:57 -0300

    ClienteModel created

---
+++ front-end/src/main/java/br/edu/utfpr/apresentacao/ClienteModel.java
@@ -0,0 +1,10 @@
+package br.edu.utfpr.apresentacao;
+
+public class ClienteModel {
+    private Long id;
+    private String nome;
+    private int idade;
+    private String telefone;
+    private double limiteCredito;
+    private PaisModel pais;
+}
